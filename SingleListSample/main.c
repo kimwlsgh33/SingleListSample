5c4b8f649e075bfdca68b091517ecfd30eb54708
이시바 <jinho971031@gmail.com>
2023-02-26 14:42:47 +0900

    Initial Commit

---
+++ SingleListSample/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SingleListSample
+//
+//  Created by 김진호 on 2023/02/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
